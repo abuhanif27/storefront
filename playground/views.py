86f247d09188b9307cc6a7e642cff9f03baea654
Abu Hanif <ahanif2500@gmail.com>
2026-05-27 04:00:42 +0600

    Refactor views and update settings; add requirements file

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
-
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
+    product = Product.objects.filter(pk=1).first()
+    print(product)
     return render(request, 'hello.html', {'name': 'Mosh'})
