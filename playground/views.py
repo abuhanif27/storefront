5a9d625afb842b1452ee59c31d00623ab08a3968
Abu Hanif <ahanif2500@gmail.com>
2026-05-31 03:14:07 +0600

    Add custom admin configurations, update templates, and include favicon

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product
+from django.db.models import F, Value, Func, Q,Value,ExpressionWrapper, DecimalField
+from django.db.models.aggregates import Count, Min, Max, Avg
+from django.db import transaction, connection
+from django.db.models.functions import Concat
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product,Customer,Order,OrderItem,Address
+from tags.models import TaggedItem
 
 
 
 def say_hello(request):
-    product = Product.objects.filter(pk=1).first()
-    print(product)
+    with connection.cursor() as cursor:
+        queryset = cursor.execute("SELECT * FROM store_collection")
+        cursor.callproc("get_customers", [1, 2])
+        print(queryset)
+        
+        
+        
+    
     return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'storefront.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
