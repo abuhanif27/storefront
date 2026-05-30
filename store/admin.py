2e3b0bca22cba41e9ebd1f026f70974c887d6eea
Abu Hanif <ahanif2500@gmail.com>
2026-05-31 05:30:19 +0600

    Add InventoryFilter and clear_inventory action to ProductAdmin

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,33 @@
-from django.contrib import admin
+from django.contrib import admin,messages
 from django.db.models import Count
 from django.utils.html import format_html,urlencode
 from django.urls import reverse
 from . import models
 # Register your models here.
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    LOW = "<10"
+    MEDIUM = "<20"
+    HIGH = ">=20"
+    def queryset(self, request, queryset):
+        if self.value() == self.LOW:
+            return queryset.filter(inventory__lt=10)
+        elif self.value() == self.MEDIUM:
+            return queryset.filter(inventory__lt=20, inventory__gte=10)
+        elif self.value() == self.HIGH:
+            return queryset.filter(inventory__gte=20)
+
+    def lookups(self, request, model_admin):
+        return [
+            (self.LOW, 'Low')
+            , (self.MEDIUM, 'Medium')
+            , (self.HIGH, 'High')
+        ]
+
+
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
@@ -23,14 +47,17 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status','collection_title']
+    actions = ['clear_inventory']
+    list_display = ['title', 'unit_price', 'inventory','inventory_status','collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
     list_select_related = ['collection']
+    list_filter = ['collection','last_update',InventoryFilter]
 
-    
+    @admin.display(ordering='collection__title')
     def collection_title(self, product):
-        return product.collection.title
+        url = reverse('admin:store_collection_changelist') + '?' + urlencode({'id': str(product.collection.id)})
+        return format_html('<a href="{}">{}</a>', url, product.collection.title)
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -38,12 +65,18 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(request, f'{updated_count} products were successfully updated.', messages.ERROR) 
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership','orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders_count(self, customer):
