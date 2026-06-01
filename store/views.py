70f3dbd836a07aba28fbca2ee731b7c773fe0ebe
Abu Hanif <ahanif2500@gmail.com>
2026-06-02 00:28:45 +0600

    Implement product listing and detail views; add REST framework settings and product serializer

--- store/views.py
+++ store/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from . import models
+from . import serializers
 
 # Create your views here.
 
 @api_view()
 def products_list(request):
-    return Response("Ok")
+    products = models.Product.objects.all()
+    serializer = serializers.ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, pk):
-    return Response(f"Product with id {pk}")
+    product = get_object_or_404(models.Product, pk=pk)
+    serializer = serializers.ProductSerializer(product)
+    return Response(serializer.data)
--- storefront/settings.py
+++ storefront/settings.py
@@ -141,3 +141,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
