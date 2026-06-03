a59fa7bc2777c0f42a69ed5d5883db6f83e36819
Abu Hanif <ahanif2500@gmail.com>
2026-06-04 00:39:22 +0600

    Enhance ProductSerializer fields and improve products_list and product_detail views for better data handling and response status

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from . import models
 from . import serializers
 
@@ -15,15 +16,21 @@ def products_list(request):
     elif request.method == 'POST':
         serializer = serializers.ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        return Response('OK')
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET','PATCH'])
 def product_detail(request, pk):
     product = get_object_or_404(models.Product, pk=pk)
-    serializer = serializers.ProductSerializer(product,context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = serializers.ProductSerializer(product,context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'PATCH':
+        serializer = serializers.ProductSerializer(product, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
 @api_view()
 def collection_list(request):
