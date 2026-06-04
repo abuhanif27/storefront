e7f54c796d95d564308102b249977b64118fb1ab
Abu Hanif <ahanif2500@gmail.com>
2026-06-04 16:05:12 +0600

    Refactor views to improve destroy method handling in ProductViewSet and CollectionViewSet for better error responses and code clarity

--- store/views.py
+++ store/views.py
@@ -15,24 +15,19 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
     
-    
-    def destroy(self, request, pk):
-        product = get_object_or_404(models.Product, pk=pk)
-        if product.orderitem_set.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if models.OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        self.perform_destroy(product)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
     
     
 class CollectionViewSet(ModelViewSet):
     queryset = models.Collection.objects.annotate(products_count=Count('product')).all()
     serializer_class = serializers.CollectionSerializer
     
-    def destroy(self, request, pk):
-        collection = get_object_or_404(models.Collection, pk=pk)
-        if collection.product_set.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if models.Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        self.perform_destroy(collection)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
     
     
