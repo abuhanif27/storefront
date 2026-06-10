890f1ace9bbfc2520348cba1a833f605c5935cc2
Abu Hanif <ahanif2500@gmail.com>
2026-06-10 16:48:20 +0600

    Add UserAdmin class to manage User model in admin interface

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from tags.models import TaggedItem
 from store.models import Product
 from store.admin import ProductAdmin
-
+from .models import User
 # Register your models here.
 class TagInline(GenericTabularInline):
     model = TaggedItem
@@ -18,3 +19,16 @@ class CustomProductAdmin(ProductAdmin):
 
 admin.site.unregister(Product)
 admin.site.register(Product, CustomProductAdmin)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2","email","first_name", "last_name"),
+            },
+        ),
+    )
