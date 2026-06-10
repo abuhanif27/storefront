1e7702649c3fd053aefab998bc2be41f19c5956a
Abu Hanif <ahanif2500@gmail.com>
2026-06-11 00:54:41 +0600

    Add Djoser for authentication and configure JWT settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
     'playground',
     'debug_toolbar',
+    'djoser',
     'store',
     'tags',
     'likes',
@@ -146,8 +147,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
-    
+     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 
 AUTH_USER_MODEL = 'core.User'
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
